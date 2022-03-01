c3b9954e6c90b73bad7c300ea109c412ca95179d
erik streller <erik.lilp@gmail.com>
2022-03-01 19:03:29 +0100

    First commit

---
+++ components/Seo.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+export default function Seo(props) {
+  const { ...customMeta } = props;
+
+  const meta = {
+    title: 'Pokédex',
+    description: 'Explore your favorite Pokémons.',
+    ...customMeta
+  };
+
+  return (
+    <Head>
+      <title>{meta.title}</title>
+      <meta name='description' content={meta.description} />
+      <link rel='shortcut icon' href='/favicon.ico' />
+    </Head>
+  );
+}
