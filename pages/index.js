29823562b8c56b1f324ed1cf6026732963027a28
erik streller <erik.lilp@gmail.com>
2022-03-02 00:54:52 +0100

    Add design for landing page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,46 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import background from '../assets/landing.jpg';
 import Seo from '../components/Seo';
+
+const landingImage = '/../assets/landing.jpg';
 
 export default function Home() {
   return (
     <>
       <Seo />
-      <main>
-        <div>Pokédex</div>
+      <main className='flex h-screen w-full flex-col items-start justify-center bg-black'>
+        <Image
+          priority
+          className='opacity-20'
+          src={background}
+          alt='Pokédex'
+          layout='fill'
+          objectFit='cover'
+          objectPosition='center'
+        />
+        <div className='z-10 mx-auto w-[90%] max-w-[1100px] text-white'>
+          <div className='mb-8 text-7xl font-bold'>Pokédex</div>
+          <p className='mb-16 max-w-2xl text-3xl'>
+            Did you know that the first Pokémon ever designed was{' '}
+            <Link href='/'>
+              <a className='text-primary-800 hover:underline hover:underline-offset-2'>
+                <span>Rhydon</span>
+              </a>
+            </Link>
+            ?
+          </p>
+          <div className='mt-8 flex'>
+            <div className='group relative'>
+              <div className='absolute -inset-1 rounded-lg bg-gradient-to-r from-primary-700 to-primary-800 opacity-75 blur transition duration-700 group-hover:opacity-100 group-hover:duration-200' />
+              <Link href='/'>
+                <a className='inline-flex scale-100 rounded-lg border border-gray-600 bg-black px-4 py-2 text-xl font-bold shadow-sm transition duration-100 hover:scale-[1.03] active:scale-[0.97]'>
+                  Explore
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
 module.exports = {
   content: ['./pages/**/*.js', './components/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans]
+      },
+      colors: {
+        primary: {
+          // Customize it on globals.css :root
+          200: withOpacity('--tw-clr-primary-200'),
+          300: withOpacity('--tw-clr-primary-300'),
+          400: withOpacity('--tw-clr-primary-400'),
+          500: withOpacity('--tw-clr-primary-500'),
+          700: withOpacity('--tw-clr-primary-700'),
+          800: withOpacity('--tw-clr-primary-800'),
+          900: withOpacity('--tw-clr-primary-900')
+        }
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
