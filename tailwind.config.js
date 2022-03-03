ba3d54b31649f4343a54763abf9e19145d1be2b6
erik streller <erik.lilp@gmail.com>
2022-03-03 10:50:03 +0100

    #core: add typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./pages/**/*.js', './components/**/*.js'],
+  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
   theme: {
     extend: {
       fontFamily: {
