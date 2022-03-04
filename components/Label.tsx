8ad0c42d4ee8dfdcacb29ddf153baa35722accca
erik streller <erik.lilp@gmail.com>
2022-03-04 19:50:25 +0100

    #feat: change padding

--- components/Label.tsx
+++ components/Label.tsx
@@ -10,7 +10,7 @@ export default function Label({ type }: LabelProps) {
   return (
     <div
       className={clsx(
-        'flex justify-center rounded-full border-2 py-2 px-3 align-middle text-sm transition-all duration-200 ease-in-out',
+        'flex justify-center rounded-full border-2 py-1 px-3 align-middle text-sm transition-all duration-200 ease-in-out',
         twTypeColors(type)
       )}
     >
