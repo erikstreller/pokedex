d668ad8df7d597333205930f9e7ff8cabdae26fb
erik streller <erik.lilp@gmail.com>
2022-03-09 17:23:24 +0100

    feat: add card pop out effect on hover

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -18,9 +19,21 @@ export default function Card({ image, name, id }: CardProps) {
       exit={{ opacity: 0 }}
     >
       <Link href={`/pokemon/${id.toString()}`}>
-        <a className='relative flex h-48 w-48 items-center justify-center rounded-2xl border-2 border-zinc-600 bg-slate-900 text-left'>
-          <Image src={image} width={200} height={200} alt={name} />
-          <div className='absolute bottom-0 w-[188px] rounded-b-2xl px-6 py-2 text-lg font-bold backdrop-blur-lg'>
+        <a
+          className={clsx(
+            'relative flex h-48 w-48 items-center justify-center text-left',
+            'rounded-2xl border-2 border-zinc-600 bg-slate-900',
+            'transition duration-200 hover:scale-[1.02]'
+          )}
+        >
+          <Image
+            src={image}
+            width={200}
+            height={200}
+            alt={name}
+            className='transition duration-200 hover:scale-[1.05]'
+          />
+          <div className='absolute bottom-0 w-[188px] rounded-b-2xl px-6 py-2 text-lg font-bold backdrop-blur-md'>
             {Capitalize(name)}
           </div>
         </a>
