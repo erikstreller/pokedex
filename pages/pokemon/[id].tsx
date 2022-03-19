31fe71119d893dfba5eb15918a887dbeb2da936f
erik streller <erik.lilp@gmail.com>
2022-03-19 14:55:56 +0100

    feat: change layout

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -111,7 +111,8 @@ export default function PokemonSide({
   return (
     <>
       <Seo title={capitalize(name)} />
-      <main className='relative h-screen w-full bg-black'>
+      {/* TODO: min h for desktop if stats */}
+      <main className='h-screen w-full bg-black'>
         <Image
           priority
           className='opacity-20'
@@ -135,53 +136,73 @@ export default function PokemonSide({
         )}
         <div
           className={clsx(
-            'relative z-10 mx-auto flex h-full w-[90%] max-w-[1100px] flex-col items-start justify-center text-white',
+            'relative z-10 mx-auto flex h-full w-[90%] max-w-[1200px] text-white',
             isLoaded && 'fade-in-start'
           )}
         >
-          <p className='text-5xl font-bold' data-fade='1'>
-            {capitalize(name)}
-          </p>
-          <div className='my-4 flex flex-wrap gap-3' data-fade='2'>
-            {types.map((type, index) => (
-              <Label key={index} type={type} />
-            ))}
-          </div>
-          <div className='mt-4 mb-10 max-w-[534px] text-2xl' data-fade='3'>
-            {modifiedDescription}
-          </div>
-          <StatsButton
-            text='stats'
-            onClick={handleShowStats}
-            className={
-              blurColor
-                ? clsx(
-                    twFromTypeColors(gradientType(types, types[0])),
-                    twToTypeColors(gradientType(types, types[1]))
-                  )
-                : 'from-slate-700 to-slate-700'
-            }
-          />
-          {!showStats && (
-            <div className='absolute -right-[5%] bottom-[5%]' data-fade='5'>
-              <Image src={image} width={500} height={500} alt={name} />
-            </div>
-          )}
-          {showStats && (
-            <div className='absolute right-0 grid grid-cols-3 gap-6'>
-              {stats.map((stat, index) => (
-                <StatCircle
-                  key={index}
-                  text={stat.name}
-                  number={stat.value}
-                  className={clsx(
-                    twFromTypeColors(gradientType(types, types[0])),
-                    twToTypeColors(gradientType(types, types[1]))
-                  )}
-                />
+          <div className='flex h-full w-1/2 flex-col items-start justify-center pl-[50px]'>
+            <p className='text-5xl font-bold' data-fade='1'>
+              {capitalize(name)}
+            </p>
+            <div className='my-4 flex flex-wrap gap-3' data-fade='2'>
+              {types.map((type, index) => (
+                <Label key={index} type={type} />
               ))}
             </div>
-          )}
+            <div className='mt-4 mb-10 max-w-[534px] text-2xl' data-fade='3'>
+              {modifiedDescription}
+            </div>
+            <StatsButton
+              text='stats'
+              onClick={handleShowStats}
+              className={
+                blurColor
+                  ? clsx(
+                      twFromTypeColors(gradientType(types, types[0])),
+                      twToTypeColors(gradientType(types, types[1]))
+                    )
+                  : 'from-slate-700 to-slate-700'
+              }
+            />
+          </div>
+          {/* TODO: image animation during scale to new position */}
+          <div
+            className={clsx(
+              'flex w-1/2 flex-col items-end',
+              !showStats ? 'justify-end pb-14' : 'justify-center'
+            )}
+          >
+            <div className='flex' data-fade='5'>
+              <Image
+                src={image}
+                width={500}
+                height={500}
+                alt={name}
+                className={clsx(
+                  'transition duration-1000',
+                  !showStats ? 'scale-100' : 'scale-[0.65]'
+                )}
+              />
+            </div>
+            {/* TODO: stats opacity from 0 to 100 animation */}
+            {showStats && (
+              <div className='-mt-10 flex w-[500px] justify-center'>
+                <div className='grid grid-cols-3 gap-8'>
+                  {stats.map((stat, index) => (
+                    <StatCircle
+                      key={index}
+                      text={stat.name}
+                      number={stat.value}
+                      className={clsx(
+                        twFromTypeColors(gradientType(types, types[0])),
+                        twToTypeColors(gradientType(types, types[1]))
+                      )}
+                    />
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
         </div>
         <div
           className={clsx(
